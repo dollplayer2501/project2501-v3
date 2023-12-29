8b1c6f61a79d7b404acdd3af00dfc4b7d9214213
dollplayer2501 <kerberos.gm@gmail.com>
2023-12-30 01:32:29 +0900

    #6 fix and add contents status

---
+++ source/images/create_etc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+cp ./source/images/raw/screenshot..project2501-v2..large.png ./source/images/screenshot..project2501-v2..thumb.png
--- source/images/create_thumb.sh
+++ source/images/create_thumb.sh
@@ -5,27 +5,56 @@
 #
 
 
-margin_diagonal=400
+margin_diagonal=500
 margin_height=20
 
 
-# ./raw/screenshot..Eleventy-netlify-V2..main..large.png
-# ./raw/screenshot..Eleventy-netlify-V2..sub..large.png
-python3 ./source/images/create_thumb.py ./source/images/raw/screenshot..Eleventy-netlify-V2..main..large.png \
+#
+# screenshot..Eleventy-netlify-V2..main..large.png
+# screenshot..Eleventy-netlify-V2..sub..large.png
+#
+
+python3 ./source/images/create_thumb.py \
+    ./source/images/raw/screenshot..Eleventy-netlify-V2..main..large.png \
     ./source/images/ \
-    1200 1000 $margin_diagonal $margin_height
-python3 ./source/images/create_thumb.py ./source/images/raw/screenshot..Eleventy-netlify-V2..sub..large.png \
+    700 1000 $margin_diagonal $margin_height \
+    --middle_height 1000 --middle_location 400 \
+    --background_color '#00000000'
+
+python3 ./source/images/create_thumb.py \
+    ./source/images/raw/screenshot..Eleventy-netlify-V2..sub..large.png \
     ./source/images/ \
-    1200 1000 $margin_diagonal $margin_height
+    700 700 $margin_diagonal $margin_height \
+    --middle_height 1300 --middle_location 300 \
+    --background_color '#00000000'
 
 
+#
 # screenshot..Eleventy-test-bed..large.png
-python3 ./source/images/create_thumb.py ./source/images/raw/screenshot..Eleventy-test-bed..large.png \
+#
+
+python3 ./source/images/create_thumb.py \
+    ./source/images/raw/screenshot..Eleventy-test-bed..large.png \
     ./source/images/ \
-    1000 900 $margin_diagonal $margin_height
+    1200 1300 $margin_diagonal $margin_height \
+    --background_color '#00000000'
 
 
-# screenshot..project2501-v3..large.png
-python3 ./source/images/create_thumb.py ./source/images/raw/screenshot..project2501-v3..large.png \
+#
+# screenshot..project2501-v3-dark..large.png
+# screenshot..project2501-v3-light..large.png
+#
+
+python3 ./source/images/create_thumb.py \
+    ./source/images/raw/screenshot..project2501-v3-dark..large.png \
     ./source/images/ \
-    1200 750 $margin_diagonal $margin_height
+    1200 800 $margin_diagonal $margin_height \
+    --middle_height 1200 --middle_location 2000 \
+    --background_color '#00000000'
+
+python3 ./source/images/create_thumb.py \
+    ./source/images/raw/screenshot..project2501-v3-light..large.png \
+    ./source/images/ \
+    1200 800 $margin_diagonal $margin_height \
+    --middle_height 1200 --middle_location 2000 \
+    --background_color '#00000000'
