4dce0fb48071ea14f8b3a5761ba4385b56c0803f
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2025-03-04 14:40:50 +0900

    fix little serious bug, no copy css

--- task/_config.mjs
+++ task/_config.mjs
@@ -12,43 +12,55 @@ export const mode = gulpMode({
 export const outputPath = mode.product() ? './_product' : './_develop';
 
 export const path = {
+  //
   'scss': {
     'source': [
       './source/assets/styles/site.scss',
     ],
     'watch': './source/assets/styles/**/*.{scss,sass}',
   },
+  //
   'javascript': [
     './source/assets/scripts/minimal-theme-switcher.js',
     './source/assets/scripts/modal.js',
   ],
+  //
   'image': [
     './source/images/**/*.{jpg,jpeg,png,webp}'
   ],
+  //
   'filePaths': [
-   {
-     'source': './source/static/**/*',
-     'destination': '',
-   },
-   {
-     'source': 'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf',
-     'destination': '/assets/webfonts',
-   },
-   {
-     'source': 'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2',
-     'destination': '/assets/webfonts',
-   },
-   {
-     'source': 'node_modules/@fortawesome/fontawesome-free/css/brands.min.css',
-     'destination': '/assets/styles',
-   },
-   {
-     'source': 'node_modules/@picocss/pico/css/pico.min.css',
-     'destination': '/assets/styles',
-   },
-   {
-     'source': 'node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
-     'destination': '/assets/styles',
-   },
+    {
+      'source': './source/static/**/*',
+      'destination': '',
+    },
+    //
+    {
+      'source': 'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf',
+      'destination': '/assets/webfonts',
+    },
+    //
+    {
+      'source': 'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2',
+      'destination': '/assets/webfonts',
+    },
+    {
+      'source': 'node_modules/@fortawesome/fontawesome-free/css/brands.min.css',
+      'destination': '/assets/styles',
+    },
+    {
+      'source': 'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
+      'destination': '/assets/styles',
+    },
+    //
+    {
+      'source': 'node_modules/@picocss/pico/css/pico.min.css',
+      'destination': '/assets/styles',
+    },
+    //
+    {
+      'source': 'node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
+      'destination': '/assets/styles',
+    },
   ],
 };
